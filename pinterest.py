b69c54b610b276fc9aa607d7f0c1a75494747f0c
Francesco Scheffczyk <francesco.scheffczyk@gmail.com>
2022-04-12 23:35:32 +0200

    Add driver options to make it work in jupyter lab

--- pinterest.py
+++ pinterest.py
@@ -16,6 +16,8 @@ class Pinterest():
         self.currentdir = os.getcwd()
         options = webdriver.ChromeOptions()
         options.add_argument('headless')
+        options.add_argument('--no-sandbox')
+        options.add_argument('--disable-dev-shm-usage')
         options.add_argument('window-size=1920x1080')
         options.add_argument("disable-gpu")
         options.add_argument("--log-level=3")
